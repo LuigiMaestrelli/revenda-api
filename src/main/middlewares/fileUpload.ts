6d894cbf346fab154c6c74c7f104b6baf66ad2eb
Luigi Maestrelli <lgmaestrelli@gmail.com>
2021-05-29 13:08:43 -0300

    chore: set upload size limit

--- src/main/middlewares/fileUpload.ts
+++ src/main/middlewares/fileUpload.ts
@@ -3,6 +3,7 @@ import { InvalidParamError } from '@/shared/errors';
 import { isValidMimeType } from '@/shared/utils/files';
 
 const ACCEPTED_FILE_TYPES = ['image/png', 'image/jpg', 'image/jpeg'];
+const MAXSIZE_30_MB = 30 * 1024 * 1024;
 
 export const imageFileFilter = (file: Express.Multer.File, callback: FileFilterCallback): void => {
     if (!isValidMimeType(ACCEPTED_FILE_TYPES, file.mimetype)) {
@@ -13,6 +14,7 @@ export const imageFileFilter = (file: Express.Multer.File, callback: FileFilterC
 };
 
 export const imageUpload = multer({
+    limits: { fileSize: MAXSIZE_30_MB },
     storage: multer.memoryStorage(),
     fileFilter: (req, file, callback) => imageFileFilter(file, callback)
 });
